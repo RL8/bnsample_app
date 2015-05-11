0eca71f9e601a3687c898c45f8a5fdc804d5f71d
Bravo on Rails <bravonya@gmail.com>
2015-05-11 18:09:47 +0100

    Correct Signup button route

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,6 @@ test "layout links" do
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
   end
 end
