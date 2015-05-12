8e5814b8b49ed0c5f4f8e674073b1374a51c0492
Bravo on Rails <bravonya@gmail.com>
2015-05-12 09:02:55 +0100

    Add test based on Exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,7 @@ test "layout links" do
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
